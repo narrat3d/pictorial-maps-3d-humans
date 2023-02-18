5062d5b871ebf27c549fa48325f54485fbcbfee5
Raimund <sraimund@ethz.ch>
2023-02-18 20:54:46 +0100

    removed unused model

--- 2_3d_body_part_inference/models.py
+++ 2_3d_body_part_inference/models.py
@@ -94,34 +94,6 @@ def disn_one_stream_pose(num_pose_points):
     return [image_2d, sdf_coords, pose_coords], [sdf]
 
 
-def disn_ey_pose(num_pose_points):
-    [image_2d, sdf_coords, sdf_coords_normalized, pose_coords] = get_inputs(num_pose_points)
-    batch_size, num_samples = get_coords_shape(sdf_coords)
-    
-    [sdf_coords_xy, sdf_coords_xz, sdf_coords_yz] = split_coords(sdf_coords)
-    sdf_coords_encoded = encode_coords(sdf_coords_normalized)
-    
-    pose_coords_encoded = encode_coords(pose_coords)
-    pose_points_repeated = tf.repeat(pose_coords_encoded, num_samples, 1)    
-
-    feature_maps_from_image = encode_image(image_2d, batch_size, global_features=False)
-    
-    local_features_abridged = get_local_features_from_image_abridged(feature_maps_from_image, sdf_coords_xy, batch_size, num_samples)
-    
-    front_view, feature_maps_from_front_view = unet_decoder(feature_maps_from_image, sdf_coords_xy, "front2d")
-    top_view, feature_maps_from_top_view = unet_decoder(feature_maps_from_image, sdf_coords_xz, "top2d")
-    side_view, feature_maps_from_side_view = unet_decoder(feature_maps_from_image, sdf_coords_yz, "side2d")
-    
-    local_features_front = get_local_features_from_view(feature_maps_from_front_view, sdf_coords_xy, batch_size, num_samples)
-    local_features_top = get_local_features_from_view(feature_maps_from_top_view, sdf_coords_xz, batch_size, num_samples)
-    local_features_side = get_local_features_from_view(feature_maps_from_side_view, sdf_coords_yz, batch_size, num_samples)
-    
-    merged_features = Concatenate()([sdf_coords_encoded, local_features_abridged, local_features_front, local_features_top, local_features_side, pose_points_repeated])
-
-    sdf = decode_features(merged_features, name="sdf3d")
-    return [image_2d, sdf_coords, pose_coords], [sdf, front_view, top_view, side_view]
-
-
 BUILDS = {
     "disn_two_stream": disn_two_stream,
     "disn_two_stream_pose": disn_two_stream_pose,
