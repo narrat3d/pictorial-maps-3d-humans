fabc67b7b01958208c1d007867e49009385f8409
Raimund <sraimund@ethz.ch>
2023-02-18 20:57:42 +0100

    added two metrics for evaluation

--- 2_3d_body_part_inference/generator.py
+++ 2_3d_body_part_inference/generator.py
@@ -6,16 +6,18 @@ import os
 input_folder = os.path.join(os.path.expanduser("~"), "Downloads", "tmp2")
 output_folder = os.path.join(os.path.dirname(__file__), "experiments")
 
-num_runs = 1
+num_runs = 5
 
-for body_part_name in ["left_hand", "left_upper_arm", "left_lower_arm", "left_lower_leg", "left_upper_leg", "torso", "left_foot", "head"]:
-    for _ in range(num_runs):
-        for model_name in ["disn_two_stream", "disn_two_stream_pose", "disn_one_stream", "disn_one_stream_pose"]:
+for body_part_name in ["left_upper_arm"]: # , "left_upper_arm", "left_lower_arm", "left_lower_leg", "left_upper_leg", "torso", "left_foot", "head"
+    for run_nr in range(num_runs):
+        for model_name in ["disn_two_stream_pose"]: # "disn_two_stream", , "disn_one_stream", "disn_one_stream_pose"
             subprocess.run(["python", "train_and_eval.py", 
                             "--body_part_name", body_part_name, 
                             "--runs_from_generator", "True",
                             "--input_folder", input_folder,
                             "--output_folder", output_folder,
-                            "--model_name", model_name])
+                            "--model_name", model_name,
+                            "--run_nr", str(run_nr + 1),
+                            "--eval_only", "False"])
             gc.collect()
             time.sleep(10)
--- 2_3d_body_part_inference/train_and_eval.py
+++ 2_3d_body_part_inference/train_and_eval.py
@@ -4,6 +4,7 @@ from models import BUILDS
 from tensorflow.keras import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger
+import tensorflow as tf
 
 import math
 import os
@@ -13,23 +14,6 @@ from data_generators import train_data_generator, eval_data_generator
 from visualize import show_results
 
 current_folder = os.path.dirname(__file__)
-
-
-def create_output_folder():
-    os.makedirs(root_output_folder, exist_ok=True)
-    
-    run_name = body_part_name + "_" + model_name
-    run_nr = 1
-    
-    output_folder = os.path.join(root_output_folder, run_name + "_run%s")
-    
-    while(os.path.exists(output_folder % run_nr)):
-        run_nr += 1
-    
-    output_folder = output_folder % run_nr
-    os.mkdir(output_folder)
-    
-    return output_folder
 
     
 # alternative to os.path.exists, which may be slow via network drives
@@ -45,7 +29,7 @@ class File_Checker():
         return file_name in self.training_data_files[subfolder_name]
 
 
-def initialise_folders():
+def initialise_folders(input_folder, body_part_name):
     global train_subfolder_names
     global test_subfolder_names
     global num_test_subfolders
@@ -70,6 +54,8 @@ def initialise_folders():
         num_test_subfolders = 1
     else :
         num_test_subfolders = len(test_subfolder_names) * len(VIEWS)
+        
+    return train_subfolder_names, test_subfolder_names, num_test_subfolders, file_checker
 
 
 def get_weights_file_path(output_folder):
@@ -141,14 +127,26 @@ def train_and_eval(body_part_name):
     return model
 
 
+def root_mean_squared_error(y_true, y_pred):
+    return tf.math.sqrt(tf.reduce_mean(tf.math.squared_difference(y_pred, y_true))) 
+
+
+def intersection_over_union(y_true, y_pred):
+    intersection = tf.cast(tf.math.logical_and(y_true <= 0, y_pred <= 0), tf.float32)
+    union = tf.cast(tf.math.logical_or(y_true <= 0, y_pred <= 0), tf.float32)
+    iou = tf.reduce_sum(intersection) / tf.reduce_sum(union) * 100
+    return iou
+
+
 # reason: https://stackoverflow.com/questions/43055289/kerass-model-fit-is-not-consistent-with-model-evaluate
-def evaluate(body_part_name):
+def evaluate(body_part_name, output_folder):
     model = create_model(model_name, body_part_name)
     use_pose_points = len(model.inputs) == 3
     
     eval_gen = eval_data_generator(input_folder, test_subfolder_names, file_checker, body_part_name, use_pose_points, eval_all_coords=True)
        
-    model.compile(loss=["mse"] * len(model.outputs), optimizer=Adam(learning_rate=LEARNING_RATE))
+    model.compile(loss=["mse"] * len(model.outputs), metrics=[root_mean_squared_error, intersection_over_union], 
+                  optimizer=Adam(learning_rate=LEARNING_RATE))
     
     best_weights_file_path = get_best_weights_file_path(output_folder)   
     model.load_weights(best_weights_file_path)
@@ -156,14 +154,14 @@ def evaluate(body_part_name):
     verbosity = 2 if runs_from_generator else 1
     
     output_folder_name = os.path.basename(output_folder)
-    csv_log_file_path = os.path.join(output_folder, "%s_eval.csv" % output_folder_name)
+    csv_log_file_path = os.path.join(output_folder, "%s_metrics.csv" % output_folder_name)
 
     result = model.evaluate_generator(eval_gen, steps=num_test_subfolders, 
                                       verbose=verbosity)
 
     # CSV logger does not seem to work: https://github.com/keras-team/keras/issues/9484
     with open(csv_log_file_path, "w") as csv_file:
-        csv_file.write("val_loss\n%s" % result)
+        csv_file.write("mse;rmse;iou\n%s" % ";".join(map(str, result)))
 
     return result
     
@@ -172,22 +170,31 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--body_part_name', dest='body_part_name', default="left_hand")
     parser.add_argument('--runs_from_generator', dest='runs_from_generator', default=False)
-    parser.add_argument('--model_name', dest='model_name', default="disn_one_stream")
+    parser.add_argument('--model_name', dest='model_name', default="disn_one_stream_pose")
     parser.add_argument('--input_folder', dest='input_folder', default=os.path.join(os.path.expanduser("~"), "Downloads", "tmp2"))
     parser.add_argument('--output_folder', dest='output_folder', default="output")
+    parser.add_argument('--run_nr', dest='run_nr', default=1)
+    parser.add_argument('--eval_only', dest='eval_only', default=False)
     
     args = parser.parse_args()
     body_part_name = args.body_part_name
-    runs_from_generator = bool(args.runs_from_generator)
+    runs_from_generator = args.runs_from_generator == "True"
     model_name = args.model_name
     input_folder = args.input_folder
     root_output_folder = args.output_folder
+    eval_only = args.eval_only == "True"
+    run_nr = args.run_nr
     
-    initialise_folders()
+    initialise_folders(input_folder, body_part_name)
+    output_folder = os.path.join(root_output_folder, body_part_name + "_" + model_name + "_run%s" % run_nr)
     
-    output_folder = create_output_folder()
-    model = train_and_eval(body_part_name)
-    weights_file_path = get_best_weights_file_path(output_folder)
-    model.load_weights(weights_file_path)
-    show_results(input_folder, test_subfolder_names, output_folder, file_checker,
-                 model, body_part_name, VIEWS, runs_from_generator=runs_from_generator)
+    if (eval_only):
+        evaluate(body_part_name, output_folder)
+    
+    else:
+        os.makedirs(output_folder, exist_ok=True)
+        model = train_and_eval(body_part_name)
+        weights_file_path = get_best_weights_file_path(output_folder)
+        model.load_weights(weights_file_path)
+        show_results(input_folder, test_subfolder_names, output_folder, file_checker,
+                     model, body_part_name, VIEWS, runs_from_generator=runs_from_generator)
