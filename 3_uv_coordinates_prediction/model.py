5f9a734690532dd6839becdb85710693aabeec3c
Raimund <sraimund@ethz.ch>
2023-12-13 12:01:06 +0100

    tab to space

--- 3_uv_coordinates_prediction/model.py
+++ 3_uv_coordinates_prediction/model.py
@@ -60,22 +60,22 @@ def up(num_filters, layer, other_layer):
 
 
 def create_model(image_size, num_body_parts, num_filters):
-	depth_image = Input((image_size, image_size, num_body_parts))
+    depth_image = Input((image_size, image_size, num_body_parts))
 
-	conv1 = Conv2D(num_filters, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(depth_image)
-	conv2 = down(num_filters, conv1)
-	conv3 = down(num_filters * 2, conv2)
-	conv4 = down(num_filters * 4, conv3)
-	conv5 = down(num_filters * 8, conv4)
+    conv1 = Conv2D(num_filters, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(depth_image)
+    conv2 = down(num_filters, conv1)
+    conv3 = down(num_filters * 2, conv2)
+    conv4 = down(num_filters * 4, conv3)
+    conv5 = down(num_filters * 8, conv4)
 
-	up4 = up(num_filters * 8, conv5, conv4)
-	up3 = up(num_filters * 4, up4, conv3)
-	up2 = up(num_filters * 2, up3, conv2)
-	up1 = up(num_filters * 1, up2, conv1)
+    up4 = up(num_filters * 8, conv5, conv4)
+    up3 = up(num_filters * 4, up4, conv3)
+    up2 = up(num_filters * 2, up3, conv2)
+    up1 = up(num_filters * 1, up2, conv1)
 
-	uv_image = Conv2D(3, 1, activation = 'sigmoid')(up1)
+    uv_image = Conv2D(3, 1, activation = 'sigmoid')(up1)
 
-	model = Model(inputs=depth_image, outputs=uv_image)
-	model.summary()
-	
-	return model
+    model = Model(inputs=depth_image, outputs=uv_image)
+    model.summary()
+    
+    return model
